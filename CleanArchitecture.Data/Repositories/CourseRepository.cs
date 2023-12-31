63c4b7a4cc6f016baabb9e679ba56f8b17af9391
arthurgodoybd <arthur.godoy@bairesdev.com>
2023-12-31 12:08:18 -0300

    Implementation of the Service and Repository

---
+++ CleanArchitecture.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchitecture.Data.Context;
+using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Data.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private UniversityDbContext _context;
+
+        public CourseRepository(UniversityDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _context.Courses;
+        }
+    }
+}
