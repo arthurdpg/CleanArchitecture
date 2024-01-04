5e33c8beeb5e090b7a684fb27dc19394d7616fab
arthurgodoybd <arthur.godoy@bairesdev.com>
2024-01-04 08:58:19 -0300

    Implementing courses view and controller

---
+++ CleanArchitecture.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+        public CourseController(ICourseService courseService) 
+        {
+            _courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            var viewModel = _courseService.GetCourses();
+
+            return View(viewModel);
+        }
+    }
+}
