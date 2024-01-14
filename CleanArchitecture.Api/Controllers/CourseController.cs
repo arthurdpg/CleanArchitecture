4bc92739ee910abd411dadb925cdf6f7ee8b224f
arthurgodoybd <arthur.godoy@bairesdev.com>
2024-01-14 00:41:22 -0300

    Implementing Post action

--- CleanArchitecture.Api/Controllers/CourseController.cs
+++ CleanArchitecture.Api/Controllers/CourseController.cs
@@ -1,3 +1,5 @@
+using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Application.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchitecture.Api.Controllers
@@ -6,6 +8,19 @@ namespace CleanArchitecture.Api.Controllers
     [Route("[controller]")]
     public class CourseController : ControllerBase
     {
-        
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CourseViewModel courseViewModel)
+        {
+            _courseService.Create(courseViewModel);
+
+            return Ok(courseViewModel);
+        }
     }
 }
